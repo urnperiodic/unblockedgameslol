23e7f4dfd2c5137758981a02494df7a60c7cbf7b
Classroomgoogle <tharaniidaranthamizhmani@gmail.com>
2026-07-05 18:12:42 -0400

    Update ChatWorkspace.jsx

--- src/components/ChatWorkspace.jsx
+++ src/components/ChatWorkspace.jsx
@@ -1,13 +1,13 @@
-export default function ChatWorkspace() {
+export default function UserChat({ onClose }) {
   return (
     <div id="chat-workspace-container" className="relative w-full h-full flex flex-col bg-[#0c0f16] text-[#e2e8f0] overflow-hidden">
       {/* Full width/height Iframe container */}
       <div id="chat-iframe-wrapper" className="flex-1 w-full h-full bg-black">
-        <iframe 
+        <iframe
           id="chat-decoy-iframe"
-          src="https://urnperiodic.github.io/extrastuffforwebsite/" 
+          src="https://urnperiodic.github.io/chat1/"
           className="w-full h-full border-none"
-          title="Extra Stuff Workspace"
+          title="User Chat"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen"
           referrerPolicy="no-referrer"
         />
